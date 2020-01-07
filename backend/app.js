5e1a5606525252afcb65cb4a64254613c44dac75
JJGITTY2018 <jackyong@pursuit.org>
2020-01-07 11:23:03 -0500

    route production case

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/user_routes');
 var logRouter = require('./routes/log_routes');
@@ -12,14 +14,18 @@ var logRouter = require('./routes/log_routes');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+// app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../frontend/build/')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname + '/../frontend/build/')))
+}
+// app.use(express.static(path.join(__dirname, '../frontend/build/')));
 // app.use(express.static(path.join(__dirname, 'public')));
 
 
